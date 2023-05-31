0f326d49e2a9f9ff91fb926dae30a10e24f8bc3b
DreamingCodes <dev@dreaming.codes>
2023-05-31 23:06:25 +0200

    Added generatePotentialVein mixin

--- src/main/java/codes/dreaming/immersiveposres/mixin/ExcavatorHandlerMixin.java
+++ src/main/java/codes/dreaming/immersiveposres/mixin/ExcavatorHandlerMixin.java
@@ -1,4 +1,20 @@
 package codes.dreaming.immersiveposres.mixin;
 
-public class ExcavatorHandler {
+import blusunrize.immersiveengineering.api.excavator.ExcavatorHandler;
+import codes.dreaming.immersiveposres.ImmersivePositionalResources;
+import net.minecraft.util.RandomSource;
+import net.minecraft.world.level.ChunkPos;
+import net.minecraft.world.level.Level;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(ExcavatorHandler.class)
+public class ExcavatorHandlerMixin {
+
+    @Inject(at = @At("RETURN"), method = "generatePotentialVein(Lnet/minecraft/world/level/Level;Lnet/minecraft/world/level/ChunkPos;Lnet/minecraft/util/RandomSource;)V", cancellable = true, remap = false)
+    private static void generatePotentialVein(Level world, ChunkPos chunkpos, RandomSource rand, CallbackInfo ci) {
+        ImmersivePositionalResources.LOGGER.info("Hello from generatePotentialVein!");
+    }
 }
